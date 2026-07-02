7df6fc7ef8cb3e1705f2d184c47a67424074a73c
yitenchen123 <2916782430@qq.com>
2026-07-02 18:00:33 +0800

    创建 Constants.swift

---
+++ Services/Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum Constants {
+    static let appGroupId = "group.com.craftlink"
+    static let packetTunnelBundleId = "com.craftlink.app.packet-tunnel"
+    static let serverIP = "10.0.0.1"
+    static let clientIP = "10.0.0.2"
+    static let subnetMask = "255.255.255.0"
+    static let serverPort: UInt16 = 11010
+    static let maxHistoryCount = 20
+}
